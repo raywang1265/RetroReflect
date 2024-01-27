3cede05fc8b5eb58f41f66bd7cd028ef6cd2fb09
guinpen <ray.wu.wang@gmail.com>
2024-01-27 15:55:27 -0500

    styling for launch page done

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,11 +1,41 @@
 
 import LogoutButton from "./LogoutButton";
+import './index.css'
 
 const Quotes = () => {
 
     return (
-        <div>  
-            <LogoutButton />
+    <div className="launch-container">
+            <div className="home-top"></div>
+            <div className="home-bar">
+                <img src={require("./assets/RetroReflect.png")} className="logo" />
+                <div></div>
+                <div></div>
+                <LogoutButton />
+            </div>
+
+            <img src={require("./assets/rolodex-gif.gif")} className="launch-img" />
+
+            <div className="moods">
+                <div></div>
+                <div className="happy-square buttons"></div>
+                <p className="buttons">Happy</p>
+                <div className="neutral-square buttons"></div>
+                <p className="buttons">Neutral</p>
+                <div className="sad-square buttons"></div>
+                <p className="buttons">Sad</p>
+                <div className="angry-square buttons"></div>
+                <p className="buttons">Angry</p>
+                <div></div>
+            </div>
+
+
+            <div className="home-bottom">
+                <p className="home-bottom-text">info@retro.reflect.com</p>
+                <div></div>
+                <p className="home-bottom-text">Privacy Policy</p>
+                <p className="home-bottom-text">Terms and Conditions</p>
+            </div>
         </div>
     );
 };
